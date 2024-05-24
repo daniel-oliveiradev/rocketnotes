7ce3705b07fba77238be2b1a387059cb472c6a55
Daniel Oliveira <daniel.oliveiraprog@gmail.com>
2024-05-24 11:29:28 -0300

    criacao da funcao de singIn e singOut

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components'
-import GlobalStyles from './styles/global'
-import theme from './styles/theme'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
 
-import { Routes } from './routes'
+import { AuthProvider } from "./hooks/auth";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { Routes } from "./routes";
+
+import theme from "./styles/theme";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-          <Routes />
+      <GlobalStyles />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,15 +1,25 @@
-import { useState  } from 'react'
+import { useState } from "react";
 
-import { FiMail, FiLock } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { FiMail, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-import { Input } from '../../components/Input'
-import { Button } from '../../components/Button'
-import { Container, Form, Background } from './style'
+import { useAuth } from "../../hooks/auth";
 
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form, Background } from "./style";
 
-export function SingIn(){
-  return(
+export function SingIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSingIn() {
+    signIn({ email, password });
+  }
+
+  return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
@@ -17,27 +27,26 @@ export function SingIn(){
 
         <h2>Faça seu login</h2>
 
-        <Input
-          placeholder="E-mail"
-          type="text"
-          icon={FiMail}
+        <Input 
+          placeholder="E-mail" 
+          type="text" 
+          icon={FiMail} 
+          onChange={e => setEmail(e.target.value)}
         />
-        <Input
-          placeholder="Senha"
-          type="password"
-          icon={FiLock}
+
+        <Input 
+          placeholder="Senha" 
+          type="password" 
+          icon={FiLock} 
+          onChange={e => setPassword(e.target.value)}
         />
-        
-        <Button title="Entrar"/>
 
-        <Link to="/register">
-          Criar conta
-        </Link>
+        <Button title="Entrar" onClick={handleSingIn} />
 
+        <Link to="/register">Criar conta</Link>
       </Form>
 
       <Background />
-
     </Container>
   );
 }
